e019ba35b2b07a5fb09c998ed87010a45b7e8287
Blue Fulmar <martinmcd@bluefulmar.com>
2022-04-26 17:51:24 +0100

    Working on iPhone
    This reverts commit f8e863b4cef1f2b0f9e0b32e8b8e18e6f2e2743d.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Gone Birding'),
       ),
       body: const Center(
         child: Text(
